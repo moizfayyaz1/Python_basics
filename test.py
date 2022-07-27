e6a9253835d68a04510a8a499a98a98de04a2bd1
moizfayyaz1 <95619803+moizfayyaz1@users.noreply.github.com>
2022-07-27 18:25:27 +0500

    First python file
    Just a few python commands

---
+++ test.py
@@ -0,0 +1,15 @@
+import json
+"""""
+data='{"name":"ali","Age":"15"}'
+parsed= json.loads(data)
+print(parsed["name"])
+"""
+
+data2 = {
+    "player": "dembele",
+    "position": "rw",
+    "cars":["mustang","ford"],
+    "boots":("nike","adidas")
+}
+jscomp=json.dumps(data2, indent=4, sort_keys=True)
+print(jscomp)
